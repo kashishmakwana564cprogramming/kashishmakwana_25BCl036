e29217ec3793331e51a39a36553ff4a06cbefa20
kashishmakwana564cprogramming <kashishmakwana564@gmail.com>
2025-09-30 22:40:46 +0530

    Create 19.c

---
+++ lab2/19.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+int i, n;
+printf("Enter a number (n) to print your name: ");
+scanf("%d", &n);
+for (i = 0; i < n; i++) {
+printf("Patel Om Kumar Rajendra bhai\n");
+}
+return 0;
+}
+2.13 1
